9cfab1a192c1d24c9b6fc1191baf7ae7f03b65fc
cxl <755194700@qq.com>
2023-11-09 21:16:24 +0800

    types: 新增卦象类型定义

--- src/config/divination.ts
+++ src/config/divination.ts
@@ -1,3 +1,9 @@
+import { DIVINATION_SYMBOL } from '@/constants/divination'
+
+const data = Object.values(DIVINATION_SYMBOL).map(item => item.key)
+
 export type DivinationState = {
   mode: string
 }
+
+export type DivinationSymbolType = ConstantArrValues<typeof data>
--- src/constants/divination.ts
+++ src/constants/divination.ts
@@ -108,5 +108,153 @@ export const DIVINATION_SYMBOL = {
   27: {
     name: t('颐'),
     key: 'yi'
+  },
+  28: {
+    name: t('大过'),
+    key: 'daguo'
+  },
+  29: {
+    name: t('坎'),
+    key: 'kan'
+  },
+  30: {
+    name: t('离'),
+    key: 'li'
+  },
+  31: {
+    name: t('咸'),
+    key: 'xian'
+  },
+  32: {
+    name: t('恒'),
+    key: 'heng'
+  },
+  33: {
+    name: t('遁'),
+    key: 'dun'
+  },
+  34: {
+    name: t('大壮'),
+    key: 'dazhaung'
+  },
+  35: {
+    name: t('晋'),
+    key: 'jin'
+  },
+  36: {
+    name: '明夷',
+    key: 'mingyi'
+  },
+  37: {
+    name: '家人',
+    key: 'jiaren'
+  },
+  38: {
+    name: '睽',
+    key: 'kui'
+  },
+  39: {
+    name: '蹇',
+    key: 'jian'
+  },
+  40: {
+    name: '解',
+    key: 'jie'
+  },
+  41: {
+    name: '损',
+    key: 'sun'
+  },
+  42: {
+    name: '益',
+    key: 'yi_42'
+  },
+  43: {
+    name: '夬',
+    key: 'guai'
+  },
+  44: {
+    name: '姤',
+    key: 'gou'
+  },
+  45: {
+    name: '萃',
+    key: 'cui'
+  },
+  46: {
+    name: '升',
+    key: 'sheng'
+  },
+  47: {
+    name: '困',
+    key: 'kun_47'
+  },
+  48: {
+    name: '井',
+    key: 'jing'
+  },
+  49: {
+    name: '革',
+    key: 'ge'
+  },
+  50: {
+    name: '鼎',
+    key: 'ding'
+  },
+  51: {
+    name: '震',
+    key: 'zhen'
+  },
+  52: {
+    name: '艮',
+    key: 'gen'
+  },
+  53: {
+    name: '渐',
+    key: 'jian_53'
+  },
+  54: {
+    name: '归妹',
+    key: 'guimei'
+  },
+  55: {
+    name: '丰',
+    key: 'feng'
+  },
+  56: {
+    name: '旅',
+    key: 'lv_56'
+  },
+  57: {
+    name: '巽',
+    key: 'xun'
+  },
+  58: {
+    name: '兑',
+    key: 'dui'
+  },
+  59: {
+    name: '涣',
+    key: 'huan'
+  },
+  60: {
+    name: '节',
+    key: 'jie_60'
+  },
+  61: {
+    name: '中孚',
+    key: 'zhongfu'
+  },
+  62: {
+    name: '小过',
+    key: 'xiaoguo'
+  },
+  63: {
+    name: '既济',
+    key: 'jiji'
+  },
+  64: {
+    name: '未济',
+    key: 'weiji'
   }
-}
+} as const
