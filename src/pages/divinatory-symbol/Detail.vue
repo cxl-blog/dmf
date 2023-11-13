583de7f1b4cdc32e5e9f04cda1fc488d177b4542
cxl <755194700@qq.com>
2023-11-13 19:34:00 +0800

    feat: 详情接口修改

--- src/pages/divinatory-symbol/Detail.vue
+++ src/pages/divinatory-symbol/Detail.vue
@@ -4,13 +4,24 @@ import { useRouter } from '@uni-helper/uni-use'
 import SymbolImg from '@/components/divination-symbol/index.vue'
 import { divinationDetail } from '@/api/divination'
 import BizScroll from '@/components/biz-scroll/index.vue'
+import { DIVINATION_SYMBOL } from '@/constants/divination'
 
 const { t } = useI18n()
+
+type Data = {
+  id: number
+  description: string
+  name: string
+  hexagramRecord: string
+}
+
 const router = useRouter()
 const trigramsId = computed<string>(() => (unref(router.page) as any).$page.options.trigramsId)
-console.log({ router, trigramsId })
-const detail = reactive<any>({})
+const detail = reactive<Partial<Data>>({})
 const bizScrollRef = ref()
+const symbolId = computed(() => {
+  return DIVINATION_SYMBOL[detail.id!]?.key
+})
 
 onBeforeMount(() => {
   getDetail()
@@ -40,8 +51,12 @@ function getDetail() {
               <text class="wm-ht">{{ t('本卦') }}</text>
             </view>
             <view class="flex-center flex-1">
-              <SymbolImg symbol-name="qian" class="divination-img" />
-              <!-- <view class="divination-img" /> -->
+              <SymbolImg
+                v-if="symbolId"
+                :symbol-name="symbolId"
+                inactiveBgColor="#ffffff"
+                class="divination-img"
+              />
             </view>
             <View class="">
               <text class="lh-1px write-vertical-right"> {{ t('地风升') }} . {{ t('本卦') }} </text>
@@ -57,7 +72,7 @@ function getDetail() {
             <view class="relative ml-20px mr--12px flex items-center">
               <text
                 class="divination-name w-16px w-16px b-1px b-r-0px b-[#000] b-solid px-8px py-5px line-height-20px write-vertical-right write-orient-upright"
-                >{{ t('周易第6卦初六爻') }}</text
+                >{{ t('周易第6卦') }}</text
               >
             </view>
             <BizScroll
@@ -65,20 +80,14 @@ function getDetail() {
               class="scroll-container min-w-0 shrink-1!"
               indicator-active-color="#ebd478"
               :scrollViewAttrs="{
-                scrollLeft: Number.MAX_SAFE_INTEGER
+                left: Number.MAX_SAFE_INTEGER
               }"
             >
               <view
                 class="divination-detail pb-20px pt-30px write-vertical-right write-orient-upright"
               >
                 <text>
-                  <text>初六爻辞，</text>
-                  <br />
-                  {{
-                    t(
-                      '初六，不永所事，小有言，终吉象曰：不永所事，讼不可长也，虽小有言，其辨明也。'
-                    )
-                  }}
+                  {{ detail.hexagramRecord }}
                 </text>
               </view>
             </BizScroll>
@@ -100,11 +109,7 @@ function getDetail() {
         <text class="font-bold">{{ t('解释说明') }}</text>
       </view>
       <view>
-        <text> 初六：做事不能持之以恒，稍有过错，最后还是吉利的 </text>
-        <br />
-        <text
-          >《象辞》说：做事不能持之以恒，说明官司不可能打久下去。虽然稍有过错，但争议的双方的是非曲直终将辨别清楚。</text
-        >
+        <text> {{ detail.description }} </text>
       </view>
     </view>
   </view>
